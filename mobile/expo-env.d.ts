4d197d9b059d5672bb9d9c5601977139861489d3
earthkingdomuniverse-cell <earthkingdomuniverse@gmail.com>
2026-04-25 05:52:44 +0700

    Add Expo env type declarations

---
+++ mobile/expo-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="expo" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    EXPO_PUBLIC_API_URL?: string;
+    EXPO_PUBLIC_ENABLE_MOCK_MODE?: string;
+    EXPO_PUBLIC_ENABLE_ADMIN_TAB?: string;
+    EXPO_PUBLIC_APP_NAME?: string;
+  }
+}
